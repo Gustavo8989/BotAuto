f3e3b2de8ddca3149a9f07fd623cef7db9ee5444
Gustavo <gustavohenriquealves418@gmail.com>
2025-07-13 20:50:34 -0300

    .

--- code/freela.py
+++ code/freela.py
@@ -38,16 +38,4 @@ for c in range(6):
     url_workona_0_4 = f"https://www.workana.com/jobs?category=it-programming&language=pt&page={c}"
     teste = freelas() 
     teste.freelas_99(url_99)
-    teste.workana(url_workona_0_4)
-
-'''page = requests.get(url_99,headers=headers) 
-soup = BeautifulSoup(page.content, 'html.parser') 
-titulo = soup.find_all('h1')
-for titulos in titulo:
-    title_projects = titulos.find('h1')
-    link_project = titulos.find('a') 
-    print(titulos.text)
-    print(link_project)'''  
-
-
-
+    teste.workana(url_workona_0_4) 
