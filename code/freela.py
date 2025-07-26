e0c2fdf87f173dac4008adfc654880840cf782af
Gustavo <gustavohenriquealves418@gmail.com>
2025-07-26 11:14:48 -0300

    Arrumando o webscraping

--- code/freela.py
+++ code/freela.py
@@ -1,41 +1,44 @@
 from bs4 import BeautifulSoup
-import pandas as pd
-import numpy as np
+from time import sleep
 import selenium 
-import urllib 
 import requests
 import re
 
 # URLs 
 headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36'}
-
+cronometro = 10
 class freelas:
     def __init__(self):
         pass 
-
     def freelas_99(self,url):
         response = requests.get(url,headers=headers)
         soup = BeautifulSoup(response.content,'html.parser')
         titulo = soup.find_all('h1')
-        for pags in url:
-            for titulos in titulo:
-                titulo_projetos = titulos.find('h1')
-                link_project = titulos.find('a')
-                project_title = titulos.text
-                print(project_title)
-                print(link_project)
+        for titulos in titulo:
+            project_title = titulos.find('h1')
+            link_project = titulos.find('a')
+            project_title = titulos.text
+            print(project_title)
+            print(link_project)
+            print('-'*30)
 
     def workana(self,url):
+        sleep(5) # Quando for para produção colocar 1 minuto 
         response_workana = requests.get(url,headers=headers)
         soup_workana = BeautifulSoup(response_workana.content,'html.parser')
-        project = soup_workana.find_all('span')
+        project = soup_workana.find_all('h1')
         for projects in project:
-            projects_ = projects.find('span')
-           # print(projects.text) 
+            projects_title_w = projects.find('h1')
+            link = projects.find('a')
+            projects_title_w = projects.text
+            print(projects_title_w)
+            print(link)
+            print('-'*30)
 
-for c in range(6):
-    url_99 = f"https://www.99freelas.com.br/projects?order=numero-de-interessados-menor&categoria=web-mobile-e-software={c}"
+
+for c in range(4):
+    url_99 = f"https://www.99freelas.com.br/projects?order=numero-de-interessados-menor&page={c}"
     url_workona_0_4 = f"https://www.workana.com/jobs?category=it-programming&language=pt&page={c}"
     teste = freelas() 
     teste.freelas_99(url_99)
-    teste.workana(url_workona_0_4) 
+    teste.workana(url_workona_0_4)
--- code/teste.py
+++ code/teste.py
@@ -9,14 +9,21 @@ import re
 # URLs
 headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36'}
 
-for c in range(6):
-    url_99 = f"https://www.99freelas.com.br/projects?order=numero-de-interessados-menor&categoria=web-mobile-e-software={c}"
+for c in range(4):
+    url_99 = f"https://www.99freelas.com.br/projects?order=numero-de-interessados-menor&page={c}"
     url_workona_0_4 = f"https://www.workana.com/jobs?category=it-programming&language=pt&page={c}"
-    page = requests.get(url_99,headers=headers)
-    soup = BeautifulSoup(page.content, 'html.parser')
-    titulo = soup.find_all('h1')
-    for titulos in titulo:
+    page_99 = requests.get(url_99,headers=headers)
+    page_workana = requests.get(url_workona_0_4,headers=headers)
+    soup_99 = BeautifulSoup(page_99.content, 'html.parser')
+    soup_workana = BeautifulSoup(page_workana.content,'html.parser')
+    titulo_99 = soup_99.find_all('h1')
+    titulo_workana = soup_workana.find_all('h1')
+    for titulos in titulo_99:
         title_projects = titulos.find('h1')
         link_project = titulos.find('a')
         print(titulos.text)
-        print(link_project)
+    print('-'*30)
+    for c in titulo_workana:
+        title_projects = c.find('h1')
+        link_project = c.find('h1')
+        print(c.text)
