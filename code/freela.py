6a6feeb49e7849cc7ca896cbb95ce54b3b583dfb
Gustavo <gustavohenriquealves418@gmail.com>
2025-07-27 17:57:54 -0300

    Inicializando o bot

--- code/freela.py
+++ code/freela.py
@@ -4,46 +4,62 @@ import telebot
 import requests
 import re
 
-# URLs 
+# FAZE DE TESTE
+# LEMBRAR DEPOIS DE ARRUMAR A ESTRUTURA DO CÓDIGO, O OBJETIVO E FAZER RODAR
+
 headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36'}
 cronometro = 10
-class freelas:
+def freela():
+    # Laço de repetição para acessar as paginas do site 
+    urls_99 = [f"https://www.99freelas.com.br/projects?order=numero-de-interessados-menor&page={c}" for c in range(4)]
+    urls_workona_0_4 = [f"https://www.workana.com/jobs?category=it-programming&language=pt&page={c}" for c in range(4)] 
+    title_projects_99 = []
+    title_projects_workana = []
+    link_project_99 = []
+    link_project_workana = []
+
+    # Abrindo a chave e inicializando o bot
     with open("auth.txt",'r') as arquivo:
         key = arquivo.read()
     TOKEN = key
     global bot 
     bot = telebot.TeleBot(TOKEN)
 
-    def freelas_99(self,url):
+    # Webscraaping na 99 freelas
+    for url in urls_99:
         response = requests.get(url,headers=headers)
         soup = BeautifulSoup(response.content,'html.parser')
         titulo = soup.find_all('h1')
         for titulos in titulo:
             project_title = titulos.find('h1')
-            self.link_project = titulos.find('a')
-            self.project_title = titulos.text
-    def workana(self,url):
-        sleep(5) # Quando for para produção colocar 1 minuto 
+            link_project = titulos.find('a')
+            project_title = titulos.text
+            title_projects_99.append(project_title)
+            link_project_99.append(link_project)
+
+    # Webscraaping na Workana
+    for url in urls_workona_0_4:
         response_workana = requests.get(url,headers=headers)
         soup_workana = BeautifulSoup(response_workana.content,'html.parser')
         project = soup_workana.find_all('h1')
         for projects in project:
             projects_title_w = projects.find('h1')
-            self.link = projects.find('a')
-            self.projects_title_w = projects.text
-            
-    @bot.massage_handler(commands=['start'])
+            link_w = projects.find('a')
+            projects_title_w = projects.text
+            title_projects_workana.append(projects_title_w)
+            link_project_workana.append(link_w)
+    
+    @bot.message_handler(commands=['start'])
     def send_projects(message):
-        bot.reply_to(message,'Teste')
-    @bot.message_handler(func=lambda msg:True)
+        bot.reply_to(message,'Você quer ver da 99 Freelas ou da Workana')
+
+    @bot.message_handler(commands=['99 Freelas'])
     def echoa_menssage(message):
-        bot.reply_to(message,message.text)
+        for projects in title_projects_99:
+            bot.reply_to(message,projects)
 
+    
+    bot.infinity_polling()
 
-
-for c in range(4):
-    url_99 = f"https://www.99freelas.com.br/projects?order=numero-de-interessados-menor&page={c}"
-    url_workona_0_4 = f"https://www.workana.com/jobs?category=it-programming&language=pt&page={c}"
-    teste = freelas() 
-    teste.freelas_99(url_99)
-    teste.workana(url_workona_0_4)
+teste = freela()
+teste 
