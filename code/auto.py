ea21875fa59ee26fb5a537f4e6f555263f66f888
Gustavo <gustavohenriquealves418@gmail.com>
2025-08-06 21:44:18 -0300

    Começo da expressao regular

--- code/auto.py
+++ code/auto.py
@@ -9,20 +9,29 @@ import re
 # LEMBRAR DEPOIS DE ARRUMAR A ESTRUTURA DO CÓDIGO, O OBJETIVO POR AGORA È FAZER RODAR
 
 headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36'}
-oferta_merca = "https://www.mercadolivre.com.br/ofertas#nav-header"
+mercado_livre = "https://www.mercadolivre.com.br/ofertas#nav-header"
 amazon = "https://www.amazon.com.br/deals?ref_=nav_cs_gb"
+BanWords = ['span','tag','style','true']
 # Expressão Regular
 def RegularExpression(x:str):
-    NameProdocts = re.findall(r"[a-zA-Z]",x)
+    NameProdocts = re.findall(r"\w[a-zA-Z]+",x)
+    LinkProdocts = re.findall(r'href="([^"]+)"',x)
+    PriceProducts = re.findall(r"\d+",x)
+    NameStore = re.findall(r">([^<]+)<",x)
+
 
 def MercadoLivre():
-    resposta_mercadolivre = requests.get(oferta_merca,headers=headers)
+    resposta_mercadolivre = requests.get(mercado_livre,headers=headers)
     soup = BeautifulSoup(resposta_mercadolivre.content,'html.parser')
     NameProdoct = str(soup.find_all("h3",class_="poly-component__title-wrapper"))
     NameStore = soup.find_all("span",class_="poly-component__brand")
-    Link = soup.find_all("a",class_="poly-component__title")
+    Link = str(soup.find("a",class_="poly-component__title"))
     Price = soup.find_all("div",class_="poly-price__current")
+    OriginalPrice = soup.find_all("s",class_="andes-money-amount andes-money-amount--previous andes-money-amount--cents-comma")
     itens_grupy = soup.find_all("div",class_="items-with-smart-groups")
+
+
+    
 def Amazon():
     responsta_amazon = requests.get(amazon,headers=headers)
     soup = BeautifulSoup(responsta_amazon.content,'html.parser')
@@ -33,7 +42,6 @@ def Amazon():
     print(NameProdoct)
 
 MercadoLivre()
-
 '''
 TIRANDO PRINT EM SELENIUM
 
