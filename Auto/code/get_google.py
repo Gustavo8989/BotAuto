273d0ce1798058b958000872460e75f67fa43a8a
Gustavo <gustavohenriquealves418@gmail.com>
2025-11-02 21:42:14 -0300

    Inicio WebScraping no google

---
+++ Auto/code/get_google.py
@@ -0,0 +1,10 @@
+from selenium import webdriver 
+from selenium.webdriver.common.by import By
+from playwright.sync_api import Page, expect
+import cv2 
+
+
+headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36'}
+options = webdriver.ChromeOptions()
+drive = webdriver.Chrome(options=options)
+
