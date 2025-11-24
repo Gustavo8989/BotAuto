5284875a7bd175ed63f3134ab49ed4dd4ff278e3
Gustavo <gustavohenriquealves418@gmail.com>
2025-11-24 19:01:11 -0300

    Get text in file PDF

--- PDF/main.py
+++ PDF/main.py
@@ -1,3 +1,8 @@
-class Editor:
-    def __init__(self):
-        pass 
+
+def escolha():
+    resposta = input("O que você quer fazer: ")
+    print('''
+        [1] -> Ver informações sobre o PDF
+        [2] -> Ver informações sobre os Metadados do PDF
+        [3] -> Editar o PDF
+    ''')
